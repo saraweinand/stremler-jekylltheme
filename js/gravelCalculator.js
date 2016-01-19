ad0af1affac397373b5d793dbff0026f576de822
Sara Weinand <saraweinand@gmail.com>
2016-01-19 10:49:23 -0800

    Update gravelCalculator.js

--- js/gravelCalculator.js
+++ js/gravelCalculator.js
@@ -24,7 +24,7 @@ var cubicYards = lengthYards * widthYards * depthYards;
 
   
 // 4: output cubic yards
-var requiredTons = cubicYards * 1.87;
+var requiredTons = cubicYards * 1.88;
 
 var result=Math.round(requiredTons*1000)/1000
 
